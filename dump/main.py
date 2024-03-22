85ab2eb7cdbf2009c033626aa658845859f951ff
Kirill Karavaev <102624537+h3ct0rzzz@users.noreply.github.com>
2024-03-22 23:10:11 +0300

    Added function for processing data to JSON

--- dump/main.py
+++ dump/main.py
@@ -1,36 +1,67 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
+import pandas as pd
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def read_and_process_data(self):
+        # Чтение данных из файлов pickle
+        books = pd.read_pickle("books.pkl")
+        instances = pd.read_pickle("instances.pkl")
+        admins = pd.read_pickle("admins.pkl")
+        users = pd.read_pickle("users.pkl")
+        readers = pd.read_pickle("readers.pkl")
+        language = pd.read_pickle("language.pkl")
+        genre = pd.read_pickle("genre.pkl")
+        format_df = pd.read_pickle("format.pkl")
+        authors = pd.read_pickle("authors.pkl")
+        productions = pd.read_pickle("productions.pkl")
+
+        books_dict = books.to_dict(orient="index")
+        instances_dict = instances.set_index("instanceID").T.to_dict("dict")
+        admins_dict = admins.set_index("adminID").T.to_dict("dict")
+        users_dict = users.set_index("userID").T.to_dict("dict")
+        readers_dict = readers.set_index("readerID").T.to_dict("dict")
+        language_dict = language.set_index("languageID").T.to_dict("dict")
+        genre_dict = genre.set_index("genreID").T.to_dict("dict")
+        format_dict = format_df.set_index("formatID").T.to_dict("dict")
+
+        authors_dict = {
+            row["authorId"]: {"name": row["name"], "productionID": row["productionID"]}
+            for index, row in authors.iterrows()
+        }
+
+        productions_dict = productions.set_index("productionID").T.to_dict("dict")
+
+        final_json = {
+            "books": books_dict,
+            "instances": instances_dict,
+            "admins": admins_dict,
+            "users": users_dict,
+            "readers": readers_dict,
+            "language": language_dict,
+            "genre": genre_dict,
+            "format": format_dict,
+            "authors": authors_dict,
+            "productions": productions_dict,
+        }
+
+        return final_json
+
+    
     def send_dump(self):
         self.send_response(200)
-        self.send_header('Content-type', 'application/json')
+        self.send_header("Content-type", "application/json")
         self.end_headers()
 
-        data = {
-            "books": {
-                1: {
-                    "title": "Book 1",
-                    "author": "Author 1",
-                    "year": 2022
-                },
-                2: {
-                    "title": "Book 2",
-                    "author": "Author 2",
-                    "year": 2021
-                },
-                3: {
-                    "title": "Book 3",
-                    "author": "Author 3",
-                    "year": 2020
-                }
-            }
-        }
+        data = self.read_and_process_data()
+        
         self.wfile.write(json.dumps(data).encode())
 
 
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000) -> None:
+def run(
+    server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000
+) -> None:
     server_address = ("", port)
     httpd = server_class(server_address, handler_class)
     httpd.serve_forever()
